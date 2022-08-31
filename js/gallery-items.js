87c9e8ed210c74e6a0d5b59a68c9becc8eb07ccb
OleksiiChernii <oleksii.chernii@gmail.com>
2022-08-31 20:41:32 +0300

    small fixes

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -69,26 +69,53 @@ galleryItems.forEach(element => {
   gallery.insertAdjacentElement("beforeend", insertImage(element));
 });
 
+let instance;
+
 function insertImage(parent) {
-  let element = document.createElement('a');
+  let element = document.createElement('div');
   element.classList.add('gallery__item');
-  element.classList.add('gallery__link');
-  let img = document.createElement('img');
-  img.classList.add('gallery__image');
-  img.addEventListener('click', () => showOriginalImage(parent))
-  img.setAttribute('src', parent.preview);
-  img.setAttribute('alt', parent.description)
-  element.appendChild(img);
+  let link =createLink();
+  let img = createImage(parent);
+  element.appendChild(link);
+  link.appendChild(img);
   return element;
 }
 
+function createLink(){
+  let link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.setAttribute('href', parent.original);
+  link.addEventListener('click', event => event.preventDefault());
+  return link;
+}
+
+function createImage(parent){
+  let img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.addEventListener('click', () => showOriginalImage(parent));
+  img.setAttribute('src', parent.preview);
+  img.setAttribute('alt', parent.description);
+  img.setAttribute('data-source', parent.original);
+  return img;
+}
 
 function showOriginalImage(parent) {
-  const instance = basicLightbox.create(`
+  instance = basicLightbox.create(`
     <div class="modal">
         <img src=${parent.original} alt=${parent.description}/>
     </div>
   `);
 
-  instance.show()
+  instance.show();
 }
+
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) {
+      isEscape = (evt.key === "Escape" || evt.key === "Esc");
+  } 
+  if (isEscape && instance) {
+      instance.close();
+  }
+};
